12a86b7d39a52e0376c30f0ba56607525063ff39
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2024-08-15 07:04:33 -0500

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,5 @@ import { useState } from 'react'
 import { Layout } from 'antd'
 import Logo from './components/core/logo';
 
-const {Header, Sider}= Layout
-function App() {
-  return (
-    <Layout>
-      <Sider className='sidebar'>Sidebar</Sider>
-      <Logo/>
-    </Layout>
-    
-  )
-}
-
 
 export default App
