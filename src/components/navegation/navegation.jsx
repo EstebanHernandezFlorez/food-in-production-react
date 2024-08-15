5c71208ad6a7d92135c41e91b7ddc66ffc35d974
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2024-08-15 16:07:09 -0500

    inicio empleados

--- src/components/navegation/navegation.jsx
+++ src/components/navegation/navegation.jsx
@@ -9,7 +9,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-import Employee from '../../views/module/employee/formEmployee';
+import Employee from '../../views/module/employee/indexEmploye';
 import Owner from '../../views/module/owner/owner';
 import Roles from '../../views/module/roles/roles';
 
