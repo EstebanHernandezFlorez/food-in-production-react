f394ad01b854962a07bc0afb845b4365e724cff3
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2024-08-28 09:18:20 -0500

    Estilos del menu

--- src/views/module/MenuList.jsx
+++ src/views/module/MenuList.jsx
@@ -23,17 +23,15 @@ const MenuList = ({ darkTheme }) => {
   };
 
   const itemStyle = {
-    color: '#D9A407', // Color de texto dorado
-  };
+    color: 'white',        // Color de texto blanco
+    textDecoration: 'none' // Sin decoración de texto
+  };  
 
   const submenuStyle = {
-    color: '#D9A407', // Color de texto dorado para los ítems dentro de SubMenu
+    color: 'black', // Color de texto dorado para los ítems dentro de SubMenu
+    backgroundColor: 'black'
   };
 
-  const activeItemStyle = {
-    backgroundColor: '#444444', // Color de fondo cuando el ítem está activo
-    color: '#D9A407', // Color de texto dorado cuando el ítem está activo
-  };
 
   // Funciones para manejar la visibilidad del modal
   const showConfirm = () => {
@@ -93,22 +91,22 @@ const MenuList = ({ darkTheme }) => {
           style={itemStyle} // Estilo aplicado al SubMenu
         >
           <Menu.Item key="produccion" style={submenuStyle}>
-            <Link to="/produccion" style={submenuStyle}>Producción</Link>
+            <Link to="/produccion" style={itemStyle}>Producción</Link>
           </Menu.Item>
           <Menu.Item key="orden_produccion" style={submenuStyle}>
-            <Link to="/orden_produccion" style={submenuStyle}>Orden de Producción</Link>
+            <Link to="/orden_produccion" style={itemStyle}>Orden de Producción</Link>
           </Menu.Item>
           <Menu.Item key="producto_insumo" style={submenuStyle}>
-            <Link to="/producto_insumo" style={submenuStyle}>Producto Insumo</Link>
+            <Link to="/producto_insumo" style={itemStyle}>Producto Insumo</Link>
           </Menu.Item>
           <Menu.Item key="insumo" style={submenuStyle}>
-            <Link to="/insumo" style={submenuStyle}>Insumo</Link>
+            <Link to="/insumo" style={itemStyle}>Insumo</Link>
           </Menu.Item>
           <Menu.Item key="empleados" style={submenuStyle}>
-            <Link to="/empleados" style={submenuStyle}>Empleados</Link>
+            <Link to="/empleados" style={itemStyle}>Empleados</Link>
           </Menu.Item>
           <Menu.Item key="proveedores" style={submenuStyle}>
-            <Link to="/proveedores" style={submenuStyle}>Proveedores</Link>
+            <Link to="/proveedores" style={itemStyle}>Proveedores</Link>
           </Menu.Item>
         </Menu.SubMenu>
         <Menu.SubMenu
@@ -118,13 +116,13 @@ const MenuList = ({ darkTheme }) => {
           style={itemStyle} // Estilo aplicado al SubMenu
         >
           <Menu.Item key="clientes" style={submenuStyle}>
-            <Link to="/clientes" style={submenuStyle}>Clientes</Link>
+            <Link to="/clientes" style={itemStyle}>Clientes</Link>
           </Menu.Item>
           <Menu.Item key="reservas" style={submenuStyle}>
-            <Link to="/reservas" style={submenuStyle}>Reservas</Link>
+            <Link to="/reservas" style={itemStyle}>Reservas</Link>
           </Menu.Item>
           <Menu.Item key="servicios" style={submenuStyle}>
-            <Link to="/servicios" style={submenuStyle}>Servicios</Link>
+            <Link to="/servicios" style={itemStyle}>Servicios</Link>
           </Menu.Item>
         </Menu.SubMenu>
         <Menu.Item
